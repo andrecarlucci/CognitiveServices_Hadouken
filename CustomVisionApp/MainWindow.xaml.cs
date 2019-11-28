e46119a84f3c20eda12c575ec8a2475714891cf6
andrecarlucci <andrecarlucci@gmail>
2019-11-28 21:35:46 +0100

    first commit

---
+++ CustomVisionApp/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using CustomVisionApp.Models;
+using Microsoft.Win32;
+using OpenCvSharp;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace CustomVisionApp
+{
+    public partial class MainWindow : System.Windows.Window {
+
+        private MyCamera _camera;
+        private LocalCustomVisionClient _localCustomVisionClient;
+        private BitmapImage _currentImage;
+        private string _projectName = "hadouken";
+        
+        public MainWindow() {
+            InitializeComponent();
+            _localCustomVisionClient = new LocalCustomVisionClient();
+        }
+
+        private void BStart_Click(object sender, RoutedEventArgs e) {
+            if(bStart.Content.Equals("Start")) {
+                _camera = new MyCamera();
+                _camera.NewImage = NewImage;
+                bStart.Content = "Stop";
+                Task.Factory.StartNew(() => _camera.StartCamera(0));
+            }
+            else {
+                _camera.Dispose();
+                bStart.Content = "Start";
+            }
+        }
+
+        private void BLoad_Click(object sender, RoutedEventArgs e) {
+            var op = new OpenFileDialog {
+                Title = "Select a picture",
+                Filter = @"All supported graphics|*.jpg;*.jpeg;*.png|
+                         JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|
+                         Portable Network Graphic (*.png)|*.png"
+            };
+            if (op.ShowDialog() == true) {
+                _currentImage = new BitmapImage(new Uri(op.FileName));
+                TheImage.Source = _currentImage;
+            }
+        }
+
+        private async void BDescribe_Click(object sender, RoutedEventArgs e) {
+            if(_currentImage == null) {
+                return;
+            }
+            var image = ImageHelper.ToImage(_currentImage);
+
+            string description = "Analyze...";
+            Description.Text = description;
+            description = await _localCustomVisionClient.Analyze(image, _projectName);
+            Description.Text = description;
+        }
+
+        private void NewImage(Mat frame) {
+            var bytes = frame.ToBytes(".png");
+            var result = _localCustomVisionClient.Analyze(bytes, _projectName).Result;
+            ChangeUI(() => {
+                _currentImage = ImageHelper.ToImage(bytes);
+                TheImage.Source = _currentImage;
+                TheWindow.Title = "Azure Vision: " + _camera.FramesPerSecond.ToString();
+                Description.Text = result;
+            });
+            Console.WriteLine("--> " + result);
+            //SpecialAtacks.Execute(result);
+        }
+
+        private void ChangeUI(Action action) {
+            Dispatcher.BeginInvoke(action);
+        }
+
+        private void BSave_Click(object sender, RoutedEventArgs e) {
+            var bytes = ImageHelper.ToImage(_currentImage);
+            File.WriteAllBytes("image.jpg", bytes);
+        }
+    }
+}
---
+++ CustomVisionApp/Models/CustomVisionClient.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomVisionApp.Models {
+    public class CustomVisionClient {
+        private CustomVisionPredictionClient _client;
+        public static string Url = "https://southcentralus.api.cognitive.microsoft.com";
+
+        public CustomVisionClient(string token) {
+            _client = new CustomVisionPredictionClient();
+            _client.ApiKey = token;
+            _client.Endpoint = Url;
+        }
+
+        public async Task<string> Analyze(byte[] image, string projectId) {
+            var imageStream = new MemoryStream(image);
+            var prediction = await _client.ClassifyImageAsync(new Guid(projectId), "", imageStream);
+            return prediction.Predictions.FirstOrDefault().TagName ?? "None";
+        }
+    }
+}
